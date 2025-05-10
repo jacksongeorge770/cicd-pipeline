3718b979afbf7a477b26aebb95c0c8c8dff1b556
Jackson George <49176518+jacksongeorge770@users.noreply.github.com>
2025-05-10 19:22:15 -0400

    changed port

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", helloHandler)
-	fmt.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting server on :8000")
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
