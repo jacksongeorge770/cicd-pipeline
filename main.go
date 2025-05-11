d04f9470096165aac63d62e3013cdf671aff1dac
Jackson George <49176518+jacksongeorge770@users.noreply.github.com>
2025-05-10 23:04:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World second time!")
+	fmt.Fprintf(w, "Hello, World from jenkin!")
 }
 
 func main() {
